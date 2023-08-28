7574516de35fa8dd22bc7ecf3bd1b7897337a3de
Jesse Brault <jbrault@mac.com>
2023-08-28 17:06:34 -0500

    Added forgotten exports.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,16 @@ export { InSignal, getInSignals } from './InSignal'
 export { Interlock, InterlockConfig, wireInterlocks } from './Interlock'
 export { OutSignal, getOutSignals } from './OutSignal'
 export { WiredController, getWiredController } from './WiredController'
-export { DetectorConfig, DetectorEvent, OnDetectParams, wireDetector } from './WiredDetector'
+export {
+    DetectorConfig,
+    DetectorEvent,
+    LocalDetectorConfig,
+    OnDetectParams,
+    RemoteDetectorConfig,
+    isLocalDetectorConfig,
+    isRemoteDetectorConfig,
+    wireDetector
+} from './WiredDetector'
 export { WiredReceiver, getWiredReceiver } from './WiredReceiver'
 export { signals } from './signals'
 export { EXPRESS_COLOR, LOCAL_COLOR, trainColors } from './trainColors'
