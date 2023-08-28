4995db74956b938f8a7c51e3becbb47622e28ac7
Jesse Brault <jbrault@mac.com>
2023-08-28 13:36:58 -0500

    0.2.0: WiredDetector api changes.

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # tracks
 
 A library for writing programs related to RailCraft in ComputerCraft 1.75/Minecraft 1.7.10.
+
+## Change Log
+
+-   0.1.0: August 27, 2023: introductory update. Now using TSTL modules.
+-   0.2.0: August 28, 2023: Various to the `WiredDetector` module's api.
--- src/WiredDetector.ts
+++ src/WiredDetector.ts
@@ -1,8 +1,6 @@
 import { EventBus, TimerManager } from '@jessebrault0709/cube'
 
-const FILTER_TIMER_LENGTH = 3
-
-export type DetectorEvent = {
+export interface DetectorEvent {
     detectorName: string
     minecartType: string
     minecartName: string
@@ -11,17 +9,34 @@ export type DetectorEvent = {
     destination: string
 }
 
-export type OnDetectParams = {
+export interface OnDetectParams {
     event: DetectorEvent
 }
 
-export type DetectorConfig = {
-    remoteId?: number
-    onDetect: (this: void, params: OnDetectParams) => void
+export interface DetectorConfig {
+    onDetect(params: OnDetectParams): void
 }
 
-const wireDetector = (detectorName: string, config: DetectorConfig, eventBus: EventBus, timerManager: TimerManager) => {
-    if (config.remoteId !== undefined) {
+export interface LocalDetectorConfig extends DetectorConfig {
+    detectorName: string
+    filterTimerLength: number
+}
+
+export const isLocalDetectorConfig = (config: DetectorConfig): config is LocalDetectorConfig =>
+    'detectorName' in config &&
+    typeof config.detectorName === 'string' &&
+    'filterTimerLength' in config &&
+    typeof config.filterTimerLength === 'number'
+
+export interface RemoteDetectorConfig extends DetectorConfig {
+    remoteId: number
+}
+
+export const isRemoteDetectorConfig = (config: DetectorConfig): config is RemoteDetectorConfig =>
+    'remoteId' in config && typeof config.remoteId === 'number'
+
+export const wireDetector = (config: DetectorConfig, eventBus: EventBus, timerManager: TimerManager) => {
+    if (isRemoteDetectorConfig(config)) {
         eventBus.subscribe('rednet_message', (senderId: number, msg: string) => {
             if (senderId === config.remoteId) {
                 const event = textutils.unserialize<DetectorEvent>(msg)
@@ -30,50 +45,40 @@ const wireDetector = (detectorName: string, config: DetectorConfig, eventBus: Ev
                 })
             }
         })
-    }
+    } else if (isLocalDetectorConfig(config)) {
+        let filtering = false
 
-    let filtering = false
-
-    eventBus.subscribe(
-        'minecart',
-        (
-            eventDetectorName: string,
-            minecartType: string,
-            minecartName: string,
-            primaryColor: number,
-            secondaryColor: number,
-            destination: string
-        ) => {
-            if (
-                !filtering &&
-                eventDetectorName === detectorName &&
-                minecartType === 'railcraft.cart.loco.steam.solid'
-            ) {
-                filtering = true
-                timerManager.start(FILTER_TIMER_LENGTH, () => {
-                    filtering = false
-                })
-                config.onDetect({
-                    event: {
-                        detectorName,
-                        minecartType,
-                        minecartName,
-                        primaryColor,
-                        secondaryColor,
-                        destination
-                    }
-                })
+        eventBus.subscribe(
+            'minecart',
+            (
+                eventDetectorName: string,
+                minecartType: string,
+                minecartName: string,
+                primaryColor: number,
+                secondaryColor: number,
+                destination: string
+            ) => {
+                if (
+                    !filtering &&
+                    eventDetectorName === config.detectorName &&
+                    minecartType === 'railcraft.cart.loco.steam.solid'
+                ) {
+                    filtering = true
+                    timerManager.start(config.filterTimerLength, () => {
+                        filtering = false
+                    })
+                    config.onDetect({
+                        event: {
+                            detectorName: config.detectorName,
+                            minecartType,
+                            minecartName,
+                            primaryColor,
+                            secondaryColor,
+                            destination
+                        }
+                    })
+                }
             }
-        }
-    )
-}
-
-export const wireDetectors = (
-    configs: Record<string, DetectorConfig>,
-    eventBus: EventBus,
-    timerManager: TimerManager
-): void => {
-    Object.entries(configs).forEach(([detectorName, config]) => {
-        wireDetector(detectorName, config, eventBus, timerManager)
-    })
+        )
+    }
 }
